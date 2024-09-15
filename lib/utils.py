5567a14a4fe4ed3ef5e154e4a59dac5869491547
Koos85 <koosjoosten@gmail.com>
2024-09-15 10:49:05 +0200

    python312

--- lib/utils.py
+++ lib/utils.py
@@ -1,31 +1,32 @@
 from typing import Optional, List
 
 
-def to_bool(val: Optional[int]) -> bool:
+def to_bool(val: Optional[int]) -> Optional[bool]:
     if val is None:
         return
     return bool(val)
 
 
-def to_int(val: Optional[str]) -> int:
+def to_int(val: Optional[str]) -> Optional[int]:
     if val is None:
         return
     return int(val)
 
 
-def to_float(val: Optional[str], multiply: Optional[float] = 1.0) -> float:
+def to_float(val: Optional[str], multiply: float = 1.0) -> Optional[float]:
     if val is None:
         return
     return float(val) * multiply
 
 
-def to_list_str(val: Optional[str], s: Optional[str] = ',') -> List[str]:
+def to_list_str(val: Optional[str], s: str = ',') -> Optional[List[str]]:
     if val is None or not isinstance(val, str):
         return
     return val.split(s)
 
 
-def to_percent_used(total: Optional[int], used: Optional[int]) -> float:
+def to_percent_used(total: Optional[int], used: Optional[int]
+                    ) -> Optional[float]:
     if used is None or total is None or total <= 0:
         return
     return used / total * 100
